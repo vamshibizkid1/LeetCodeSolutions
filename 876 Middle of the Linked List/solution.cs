96b14f2cee45fa248a6c5569411109b6a9b06184
vamshibizkid1 <vamshi.bizkid1@gmail.com>
2022-12-25 21:47:29 -0800

    876, 1342

---
+++ 876 Middle of the Linked List/solution.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        //slow and fast pointer method
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+
+        //Time Complexity - O(n)
+
+        //Space Complexity - O(1)
+    }
+}
